78e08972086357c2f4269ea56a37992bcc45b090
Daniel Suchy <suchydan@gmail.com>
2019-09-16 23:19:03 +0200

    Add Android support

--- android/src/main/java/com/capacitor/rateApp/CapacitorRateApp.java
+++ android/src/main/java/com/capacitor/rateApp/CapacitorRateApp.java
@@ -6,15 +6,21 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
 @NativePlugin()
 public class CapacitorRateApp extends Plugin {
 
     @PluginMethod()
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void requestReview(PluginCall call) {
+        String appPackageName = getContext().getPackageName();
+        Log.d("Capacitor", "Opening market for ".concat(appPackageName));
+        Intent marketIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id="+appPackageName));
+        marketIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_DOCUMENT | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        getContext().startActivity(marketIntent);
 
-        JSObject ret = new JSObject();
-        ret.put("value", value);
-        call.success(ret);
+        call.success();
     }
 }
