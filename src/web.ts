943bf0b89f2c5578437ddb00334c500d3aa8ef50
Ahsan <ahsan024@gmail.com>
2020-10-07 18:29:38 +0500

    added: functionality for `showInAppReview`

--- src/web.ts
+++ src/web.ts
@@ -13,6 +13,9 @@ export class CapacitorRateAppWeb extends WebPlugin
   async requestReview(): Promise<void> {
     return;
   }
+  async showInAppReview(): Promise<void> {
+    return;
+  }
 }
 
 const CapacitorRateApp = new CapacitorRateAppWeb();
