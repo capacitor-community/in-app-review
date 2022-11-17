4fceca4c624f6c5f5715dd46a8900148fa337695
Daniel Suchy <suchydan@gmail.com>
2022-11-17 23:17:18 +0100

    update example app

--- example/capacitor.config.ts
+++ example/capacitor.config.ts
@@ -1,13 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'example',
   webDir: 'dist',
   bundledWebRuntime: false,
-  server: {
-    url: 'http://localhost:5173',
-  }
 };
 
 export default config;
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,19 +1,25 @@
-
-import './App.css'
+import './App.css';
 import { RateApp } from 'capacitor-rate-app';
 
 function App() {
+  const handleRequestReview = async () => {
+    try {
+      await RateApp.requestReview();
+      alert('Review request successful');
+    } catch (error) {
+      console.log(error);
+      alert(`Review request failed: ${(error as any)?.message}`);
+    }
+  };
 
   return (
     <div className="App">
       <h1>capacitor-rate-app example</h1>
       <div className="card">
-        <button onClick={() => RateApp.requestReview()}>
-          Request review
-        </button>
+        <button onClick={handleRequestReview}>Request review</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
