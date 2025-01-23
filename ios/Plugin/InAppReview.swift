0ea25c634ec6a1e49a85909f41cc08b7f888a184
Maxime <55385993+MaximeSIMET@users.noreply.github.com>
2025-01-23 16:39:16 +0100

    Update ios/Plugin/InAppReview.swift
    Co-authored-by: Avi Leavitt <avileavitt@gmail.com>

--- ios/Plugin/InAppReview.swift
+++ ios/Plugin/InAppReview.swift
@@ -4,13 +4,14 @@ import StoreKit
 
 @objc public class InAppReview: NSObject {
     @objc func requestReview(_ call: CAPPluginCall) {
-        if #available(iOS 14.0, *) {
-             if let windowScene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
-                 SKStoreReviewController.requestReview(in: windowScene)
-             }
-         } else {
-             SKStoreReviewController.requestReview()
-         }
+        DispatchQueue.main.async {
+            if #available(iOS 14.0, *) {
+                if let windowScene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+                    SKStoreReviewController.requestReview(in: windowScene)
+                }
+            } else {
+                SKStoreReviewController.requestReview()
+            }
         call.resolve()
     }
 }
