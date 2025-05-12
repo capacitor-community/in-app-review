20e39995d7f200949e89eb29eab6254817e5cad5
jcesarmobile <jcesarmobile@gmail.com>
2025-05-12 18:19:21 +0200

    single scene

--- ios/Plugin/InAppReview.swift
+++ ios/Plugin/InAppReview.swift
@@ -5,19 +5,14 @@ import StoreKit
 @objc public class InAppReview: NSObject {
     @objc func requestReview(_ call: CAPPluginCall) {
         DispatchQueue.main.async {
-            if #available(iOS 17.0, *) {
-                if let windowScene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+            if let windowScene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+                if #available(iOS 17.0, *) {
                     AppStore.requestReview(in: windowScene)
-                }
-            } else {
-                if let windowScene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+                } else {
                     SKStoreReviewController.requestReview(in: windowScene)
                 }
             }
         }
-
         call.resolve()
     }
 }
-
-
