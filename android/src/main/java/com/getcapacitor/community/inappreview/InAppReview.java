41f8e61238de43278accd7c9da6a031615ba8b46
jcesarmobile <jcesarmobile@gmail.com>
2025-08-21 16:12:32 +0200

    fmt

--- android/src/main/java/com/getcapacitor/community/inappreview/InAppReview.java
+++ android/src/main/java/com/getcapacitor/community/inappreview/InAppReview.java
@@ -14,41 +14,31 @@ public class InAppReview {
         final ReviewManager manager = ReviewManagerFactory.create(activity);
 
         Task<ReviewInfo> request = manager.requestReviewFlow();
-        request.addOnFailureListener(
-                e -> {
+        request.addOnFailureListener(e -> {
+            e.printStackTrace();
+            call.reject("Request review failed", e);
+        });
+        request.addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                // We can get the ReviewInfo object
+                ReviewInfo reviewInfo = task.getResult();
+                Task<Void> flow = manager.launchReviewFlow(activity, reviewInfo);
+                flow.addOnCompleteListener(task1 -> {
+                    Log.i("RateApp", "Request review flow finished");
+                    // The flow has finished. The API does not indicate whether the user
+                    // reviewed or not, or even whether the review dialog was shown. Thus, no
+                    // matter the result, we continue our app flow.
+                    call.resolve();
+                });
+                flow.addOnSuccessListener(result -> call.resolve());
+                flow.addOnFailureListener(e -> {
                     e.printStackTrace();
-                    call.reject("Request review failed", e);
-                }
-        );
-        request.addOnCompleteListener(
-                task -> {
-                    if (task.isSuccessful()) {
-                        // We can get the ReviewInfo object
-                        ReviewInfo reviewInfo = task.getResult();
-                        Task<Void> flow = manager.launchReviewFlow(activity, reviewInfo);
-                        flow.addOnCompleteListener(
-                                task1 -> {
-                                    Log.i("RateApp", "Request review flow finished");
-                                    // The flow has finished. The API does not indicate whether the user
-                                    // reviewed or not, or even whether the review dialog was shown. Thus, no
-                                    // matter the result, we continue our app flow.
-                                    call.resolve();
-                                }
-                        );
-                        flow.addOnSuccessListener(
-                                result -> call.resolve()
-                        );
-                        flow.addOnFailureListener(
-                                e -> {
-                                    e.printStackTrace();
-                                    call.reject("Request review flow Failed", e);
-                                }
-                        );
-                    } else {
-                        // There was some problem, continue regardless of the result.
-                        call.reject("Request review task Failed");
-                    }
-                }
-        );
+                    call.reject("Request review flow Failed", e);
+                });
+            } else {
+                // There was some problem, continue regardless of the result.
+                call.reject("Request review task Failed");
+            }
+        });
     }
 }
