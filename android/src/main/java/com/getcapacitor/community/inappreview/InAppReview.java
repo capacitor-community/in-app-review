213b56b403e75a9a6dd054937d6dee7c6b534120
jcesarmobile <jcesarmobile@gmail.com>
2025-08-21 15:58:05 +0200

    refactor(android): replace anonymous classes with lambdas

--- android/src/main/java/com/getcapacitor/community/inappreview/InAppReview.java
+++ android/src/main/java/com/getcapacitor/community/inappreview/InAppReview.java
@@ -3,9 +3,6 @@ package com.getcapacitor.community.inappreview;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import com.getcapacitor.PluginCall;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.play.core.review.ReviewInfo;
 import com.google.android.play.core.review.ReviewManager;
@@ -18,57 +15,40 @@ public class InAppReview {
 
         Task<ReviewInfo> request = manager.requestReviewFlow();
         request.addOnFailureListener(
-            new OnFailureListener() {
-                @Override
-                public void onFailure(Exception e) {
+                e -> {
                     e.printStackTrace();
                     call.reject("Request review failed", e);
                 }
-            }
         );
         request.addOnCompleteListener(
-            new OnCompleteListener<ReviewInfo>() {
-                @Override
-                public void onComplete(Task<ReviewInfo> task) {
+                task -> {
                     if (task.isSuccessful()) {
                         // We can get the ReviewInfo object
                         ReviewInfo reviewInfo = task.getResult();
                         Task<Void> flow = manager.launchReviewFlow(activity, reviewInfo);
                         flow.addOnCompleteListener(
-                            new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(Task<Void> task) {
+                                task1 -> {
                                     Log.i("RateApp", "Request review flow finished");
                                     // The flow has finished. The API does not indicate whether the user
                                     // reviewed or not, or even whether the review dialog was shown. Thus, no
                                     // matter the result, we continue our app flow.
                                     call.resolve();
                                 }
-                            }
                         );
                         flow.addOnSuccessListener(
-                            new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void result) {
-                                    call.resolve();
-                                }
-                            }
+                                result -> call.resolve()
                         );
                         flow.addOnFailureListener(
-                            new OnFailureListener() {
-                                @Override
-                                public void onFailure(Exception e) {
+                                e -> {
                                     e.printStackTrace();
                                     call.reject("Request review flow Failed", e);
                                 }
-                            }
                         );
                     } else {
                         // There was some problem, continue regardless of the result.
                         call.reject("Request review task Failed");
                     }
                 }
-            }
         );
     }
 }
